f5eae3e3bdd1eacef0c9caaa8fcde030fa68ee61
srinidoss05-pixel <srinidoss05@gmail.com>
2025-12-21 12:26:57 +0400

    rework

--- song.js
+++ song.js
@@ -12,7 +12,7 @@ document.querySelectorAll('audio').forEach(function(audio) {
 });
 
 
-document.getElementById("MyKannazhagiii").addEventListener('play',function(){
+document.getElementById("MyKannazhagiii").addEventListener('click',function(){
 
 alert(`sri: Uyirae uyirae unaivida edhuvum
 Uyril peridhaai illayadi
@@ -21,14 +21,14 @@ Azhagil azhagaai illaiyadi💗🫴`);
 
 });
 
-document.getElementById("NeeParthaVizhigal").addEventListener('play',function(){
+document.getElementById("NeeParthaVizhigal").addEventListener('click',function(){
 
 alert('sri: Nijamadi pennae tholaivinil unnai\nNilavinil kanden nadamaada\nValiyadi pennae varaimurai illai\nVadhaikkiraai ennai medhuvaaga');
 
 
 });
 
-document.getElementById("thoseeyes").addEventListener('play',function(){
+document.getElementById("thoseeyes").addEventListener('click',function(){
 
 alert('Hey!narmu you have a hypnotising brown eyes👁️🫣');
 
@@ -36,26 +36,26 @@ alert('Hey!narmu you have a hypnotising brown eyes👁️🫣');
 });
 
 
-document.getElementById("smile").addEventListener('play',function()
+document.getElementById("smile").addEventListener('click',function()
 {
     alert("Hey! papi your the reason for my smiile 😁");
 });
 
-document.getElementById("blue").addEventListener('play',function(){
+document.getElementById("blue").addEventListener('click',function(){
 
 alert(' Sri:my love will always stay by you ☺️');
 
 
 });
 
-document.getElementById("vizhiveekura").addEventListener('play',function(){
+document.getElementById("vizhiveekura").addEventListener('click',function(){
 alert(' Sri: Unnoda konja neram unna Naanum konja venum ☺️');
 
 
 });
 
 
-document.getElementById("iwannabeyours").addEventListener('play',function(){
+document.getElementById("iwannabeyours").addEventListener('click',function(){
 alert(' Sri:i wanna be yours💗🫴 ');
 
 
